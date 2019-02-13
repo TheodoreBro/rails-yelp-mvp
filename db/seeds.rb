3ebc75b3db7d5c8ca6605a92420247a1ac655186
Theodore Brossollet <theodore.brossollet@edu.emlyon.com>
2019-02-13 15:34:04 +0100

    done

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,37 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+puts 'Cleaning database...'
+Restaurant.destroy_all
+
+puts 'Creating restaurants...'
+restaurants_attributes = [
+  {
+    name: 'les délices de pekin',
+    address: '7 rue de conichoua',
+    category: 'chinese'
+  },
+  {
+    name: 'mama roma',
+    address: '56 villa medicis',
+    category: 'italian'
+  },
+  {
+    name: 'あ  か',
+    address: '7 tokyo street',
+    category: 'japanese'
+  },
+  {
+    name: 'Le french cancan',
+    address: '56 rue saint georges',
+    category: 'french'
+  },
+  {
+    name: 'la frite à dédé',
+    address: '7 ya pas de rue à BX frère',
+    category: 'belgian'
+  }
+]
+Restaurant.create!(restaurants_attributes)
+puts 'Finished!'
